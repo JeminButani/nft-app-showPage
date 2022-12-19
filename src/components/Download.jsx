6d33ee4550f4805bbd640632aa0423266c93b682
JeminButani <95534964+JeminButani@users.noreply.github.com>
2022-12-19 13:42:00 +0530

    first commit

---
+++ src/components/Download.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import styles from "../styles/Global";
+import assets from "../assets";
+
+const Download = () => {
+  return (
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div className={styles.flexCenter}>
+          <img
+            src={assets.scene}
+            alt="download_png"
+            className={styles.fullImg}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Download;
